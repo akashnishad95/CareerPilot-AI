7abdeafaa07e2827f7acf15bdc6b9536b99208f5
akashnishad95 <akashnishad6070@gmail.com>
2026-06-28 23:51:55 +0530

    intialize frontend and add authenticastion pages

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import {RouterProvider} from "react-router";
+import {router} from "./app_routes.jsx";
+
+function App() {
+ 
+
+  return (
+    < RouterProvider router={router} />
+  )
+}
+
+export default App
