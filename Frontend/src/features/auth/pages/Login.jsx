499900f5e492b1381cf44e8e556290df7abb48eb
akashnishad95 <akashnishad6070@gmail.com>
2026-06-29 23:53:12 +0530

    create responsive login and register pages

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import {useNavigate,Link} from "react-router";
+import './auth_form.scss';
 
 const Login = () => {
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle login logic here
+  }
+
   return (
-    <div>Login</div>
+    <main>
+      <div className="form_container">
+        <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="input_group">
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" name="email" placeholder="Enter your email" />
+          </div>
+          <div className="input_group">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" name="password" placeholder="Enter your password" />
+          </div>
+          <button className="button btn-primary" type="submit">Login</button>
+        </form>
+          <p>Don't have an account? <Link to="/register">Register</Link></p>
+      </div>
+    </main>
   )
 }
 
--- Frontend/src/features/auth/pages/Register.jsx
+++ Frontend/src/features/auth/pages/Register.jsx
@@ -1,8 +1,37 @@
 import React from 'react';
+import {useNavigate,Link} from "react-router";
 
 const Register = () => {
+     const navigate = useNavigate();
+
+
+    const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle login logic here
+  }
+
   return (
-    <div>Register</div>
+    <main>
+      <div className="form_container">
+        <h1>Register</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="input_group">
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" name="username" placeholder="Enter your username" />
+          </div>
+          <div className="input_group">
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" name="email" placeholder="Enter your email" />
+          </div>
+          <div className="input_group">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" name="password" placeholder="Enter your password" />
+          </div>
+          <button className="button btn-primary" type="submit">Register</button>
+        </form>
+         <p>Already have an account? <Link to="/login">Login</Link></p> 
+      </div>
+    </main>
   )
 }
 
